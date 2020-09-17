25e74008646426594a10aa226f3b4e22c13d44b4
brilubel15 <a01022972@itesm.mx>
2020-09-17 14:21:55 -0500

    Funcion control

--- k_means.py
+++ k_means.py
@@ -1,5 +1,17 @@
 from scipy.spatial import distance
+from sklearn.cluster import KMeans
+import nompy as np
 
 def distancia(lista1,lista2):
     distancia = distance.euclidean(lista1, lista2)
     return distancia
+
+
+def centros(listas):
+    listasnonpy = np.array(listas)
+    sumaParcial = 0
+    for lista in listas:
+        sumaParcial+=lista
+    cantValores = len(listas)
+    return sumaParcial/float(cantValores)
+print promedio
