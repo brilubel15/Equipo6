c3ff9110b312051ca920b75441f4f1e1168617f6
brilubel15 <a01022972@itesm.mx>
2020-09-17 13:49:11 -0500

    .

--- k_means.py
+++
@@ -1,7 +0,0 @@
-def centros(listas):
-    sumaParcial = 0
-    for lista in listas:
-        sumaParcial+=lista
-    cantValores = len(listas)
-    return sumaParcial/float(cantValores)
-print promedio
