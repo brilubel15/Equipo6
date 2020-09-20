b81e5b63dd8a363692abbb3e8cd0e59fc2cd4f42
brilubel15 <a01022972@itesm.mx>
2020-09-19 21:12:42 -0500

    Final Arrangements

--- k_means.py
+++ k_means.py
@@ -1,5 +1,6 @@
 from scipy.spatial import distance
 from sklearn.cluster import KMeans
+from os import system
 import numpy as np
 import random
 import matplotlib.pyplot as plt
@@ -16,7 +17,7 @@ def centros(lista):
     centros_list = []
 
     # Convert the list into a numpy array
-    listasnonpy = np.array(lista)
+    listasnonpy = np.array(lista, dtype=object)
 
     for i in listasnonpy:
         centros_list.append(np.mean(i, axis = 0))
@@ -36,41 +37,52 @@ def cercanos(puntos,centros):
 def k_means(puntos):
     cantidad_de_centros = 0
     points = np.array(puntos)
-    # cantidad_de_centros = int(input("Elija la cantidad de centros: "))
-    cantidad_de_centros = 3
-    idx = np.random.randint(len(points),size=cantidad_de_centros)
 
+    cantidad_de_centros = int(input("Elija la cantidad de centros: "))
+    system('clear')
+    iterationts = int(input("Elija la cantidad de iteraciones: "))
+
+    idx = np.random.randint(len(points),size=cantidad_de_centros)
     k_lista = points[idx,:]
     clusters = cercanos(points, k_lista)
+
     # Redifine puntos and centers adjusting with the methods cercanos() and centros()
-    for i in range(100):
+    for i in range(iterationts):
         if i % 1 == 0:
             if i == 0:
                 title = "Initialization"
             else:
                 title = "Iteration {}".format(i+1)
+
         show_clusters_centroids(clusters, k_lista, title)
         clusters = cercanos(points, k_lista)
         k_lista = centros(clusters)
 
-    # Return the new adjusts list
-    return k_lista
+    # Return the new adjustes list
+    return clusters, k_lista
 
 def generarPuntos(i, j):
     puntos = []
     for cony in range(i):
         lista=[]
         for conx in range(j):
-            lista.append(random.randint(0, 100))
+            lista.append(random.randint(0, 5000))
         puntos.append(lista)
     return puntos
 
+# def generarPuntos(i):
+#     lista = []
+#     for con in range(i):
+#         lista.append(random.randint(0, 100))
+#     return np.array(lista)
+
+
 if __name__ == '__main__':
-    puntos = generarPuntos(100, 8)
-    print("Antes: ")
-    print(puntos)
-
-
+    system('clear')
+    system('clear')
+    puntos = generarPuntos(200, 8)
+    # print("Antes: ")
+    # print(puntos)
     cluster=k_means(puntos)
     print("Despues: ")
     print(cluster)
